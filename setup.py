299524321878803368d6f309df3037786fd31445
Roman Yurchak <rth.yurchak@gmail.com>
2016-10-22 11:53:57 +0200

    Switching to OS independant path in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
+import os
+
 # Complain on 32-bit systems. See README for more details
 import struct
 if struct.calcsize('P') < 8:
@@ -15,40 +17,44 @@ if struct.calcsize('P') < 8:
         'Simhash-py does not work on 32-bit systems. See README.md')
 
 ext_files = [
-    'simhash/simhash-cpp/src/permutation.cpp',
-    'simhash/simhash-cpp/src/simhash.cpp'
+    os.path.join('simhash','simhash-cpp', 'src', 'permutation.cpp'),
+    os.path.join('simhash','simhash-cpp', 'src', 'simhash.cpp')
 ]
 
 kwargs = {}
 
+print(os.listdir(os.path.join("simhash", "simhash-cpp")))
+
 try:
     from Cython.Distutils import build_ext
+
+    # Adapt extra_compile_args based on the compiler used
+    # https://stackoverflow.com/a/5192738/1791279
+    class build_ext_subclass( build_ext ):
+        def build_extensions(self):
+            c = self.compiler.compiler_type
+            if c == "msvc":
+                pass  # C++ 11 support in Visual Studio should be turned on by default
+                      # and -std=c++11 is not a valid option
+            else:
+               for e in self.extensions:
+                   e.extra_compile_args = ['-std=c++11']
+            build_ext.build_extensions(self)
+
     print('Building from Cython')
-    ext_files.append('simhash/simhash.pyx')
-    kwargs['cmdclass'] = {'build_ext': build_ext}
+    ext_files.append(os.path.join('simhash', 'simhash.pyx'))
+    kwargs['cmdclass'] = {'build_ext': build_ext_subclass}
 except ImportError:
     print('Buidling from C++')
-    ext_files.append('simhash/simhash.cpp')
+    ext_files.append(os.path.join('simhash','simhash.cpp'))
 
 ext_modules = [
     Extension('simhash.simhash', ext_files,
         language='c++',
-        include_dirs=['simhash/simhash-cpp/include']
+        include_dirs=[os.path.join('simhash','simhash-cpp','include')]
     )
 ]
 
-# Adapt extra_compile_args based on the compiler used
-# https://stackoverflow.com/a/5192738/1791279
-class build_ext_subclass( build_ext ):
-    def build_extensions(self):
-        c = self.compiler.compiler_type
-        print(c)
-        if c == "msvc":
-            pass  # C++ 11 support in Visual Studio should be turned on by default
-        else:
-           for e in self.extensions:
-               e.extra_compile_args = ['-std=c++11']
-        build_ext.build_extensions(self)
 
 
 setup(name           = 'simhash',
