ed4823e56488574358c35ee1aada262514c1e5a7
Jeff Wood <jeff@scriptedworld.com>
2019-11-15 16:24:39 -0800

    Replace implementation of shingle function.
    Old implementation blindly called next() on iterable objects without
    checking for StopIteration exceptions (exhaustion).

--- simhash/__init__.py
+++ simhash/__init__.py
@@ -5,14 +5,22 @@ from six.moves import range as six_range
 
 
 def shingle(tokens, window=4):
-    '''A generator for a moving window of the provided tokens.'''
+    """A generator for a moving window of the provided tokens."""
     if window <= 0:
-        raise ValueError('Window size must be positive')
-    its = []
-    for number in six_range(window):
-        it = iter(tokens)
-        its.append(it)
-        for _ in six_range(number):
-            next(it)
-    while True:
-        yield [next(it) for it in its]
+        raise ValueError("Window size must be positive")
+
+    # Start with an empty output set.
+    curr_window = []
+
+    # Iterate over the input tokens, once.
+    for token in tokens:
+        # Add to the window.
+        curr_window.append(token)
+
+        # If we've collected too many, remove the oldest item(s) from the collection
+        while len(curr_window) > window:
+            curr_window.pop(0)
+
+        # Finally, if the window is full, yield the data set.
+        if len(curr_window) == window:
+            yield list(curr_window)
