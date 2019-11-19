46de27b310022e3ac7a856c5fc4f4b0df1d76af7
J W <j@a1ph4g33k.com>
2019-11-19 15:57:35 -0800

    PS-7713 : Python 3 compatibility (#50)
    * Remove hardcoded version from Cython. Py3 compatibility in recent revisions.
    * Replace implementation of shingle function.
       Old implementation blindly called next() on iterable objects without checking for StopIteration exceptions (exhaustion).
    * Repinning Cython version to post Py3 compatibility fixes.
    * Black formatting applied; updated version & contact info.

--- setup.py
+++ setup.py
@@ -6,60 +6,50 @@ from distutils.extension import Extension
 
 # Complain on 32-bit systems. See README for more details
 import struct
-if struct.calcsize('P') < 8:
-    raise RuntimeError(
-        'Simhash-py does not work on 32-bit systems. See README.md')
 
-ext_files = [
-    'simhash/simhash-cpp/src/permutation.cpp',
-    'simhash/simhash-cpp/src/simhash.cpp'
-]
+if struct.calcsize("P") < 8:
+    raise RuntimeError("Simhash-py does not work on 32-bit systems. See README.md")
+
+ext_files = ["simhash/simhash-cpp/src/permutation.cpp", "simhash/simhash-cpp/src/simhash.cpp"]
 
 kwargs = {}
 
 try:
     from Cython.Distutils import build_ext
-    print('Building from Cython')
-    ext_files.append('simhash/simhash.pyx')
-    kwargs['cmdclass'] = {'build_ext': build_ext}
+
+    print("Building from Cython")
+    ext_files.append("simhash/simhash.pyx")
+    kwargs["cmdclass"] = {"build_ext": build_ext}
 except ImportError:
-    print('Buidling from C++')
-    ext_files.append('simhash/simhash.cpp')
+    print("Buidling from C++")
+    ext_files.append("simhash/simhash.cpp")
 
 ext_modules = [
     Extension(
-        'simhash.simhash', ext_files,
-        language='c++',
-        extra_compile_args=['-std=c++11'],
-        include_dirs=['simhash/simhash-cpp/include']
+        "simhash.simhash",
+        ext_files,
+        language="c++",
+        extra_compile_args=["-std=c++11"],
+        include_dirs=["simhash/simhash-cpp/include"],
     )
 ]
 
 setup(
-    name='simhash-py',
-    version='0.4.0',
-    description='Near-Duplicate Detection with Simhash',
-    url='http://github.com/seomoz/simhash-py',
-    author='Dan Lecocq',
-    author_email='dan@moz.com',
+    name="simhash-py",
+    version="0.4.1",
+    description="Near-Duplicate Detection with Simhash",
+    url="http://github.com/seomoz/simhash-py",
+    author="Moz Pro Services",
+    author_email="turbo@moz.com",
     classifiers=[
-        'Programming Language :: Python',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP'
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
     ],
     ext_modules=ext_modules,
-    packages=[
-        'simhash'
-    ],
-    package_dir={
-        'simhash': 'simhash'
-    },
-    tests_require=[
-        'coverage',
-        'nose',
-        'nose-timer',
-        'rednose'
-    ],
+    packages=["simhash"],
+    package_dir={"simhash": "simhash"},
+    tests_require=["coverage", "nose", "nose-timer", "rednose"],
     **kwargs
 )
